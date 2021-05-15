38005c7feaf9bfe92a8515c3d2861fc51dc938da
Rajkumar Dusad <32831684+rajkumardusad@users.noreply.github.com>
2021-05-15 08:06:16 +0530

    Added new android and iOS versions

--- src/UserAgent.php
+++ src/UserAgent.php
@@ -1,13 +1,13 @@
 <?php
 /**
-* User Agent Library
-* Parse userAgent information.
-*
-* @package : User Agent
-* @category : Library
-* @author : Unic Framework
-* @link : https://github.com/unicframework/unic
-*/
+ * User Agent Library
+ * Parse user agent information.
+ *
+ * @package : User Agent
+ * @category : Library
+ * @author : Unic Framework
+ * @link : https://github.com/unicframework/unic
+ */
 
 namespace UserAgent;
 
@@ -23,8 +23,8 @@ class UserAgent {
   public $referrer;
   public $isReferred;
 
-  function __construct() {
-    $this->userAgent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : NULL;
+  function __construct(string $userAgent = NULL) {
+    $this->userAgent = (isset($userAgent) ? $userAgent : (isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : NULL));
     $this->ip = $this->getIP();
     $this->os = $this->getOS();
     $this->osVersion = $this->getOsVersion();
@@ -32,15 +32,15 @@ class UserAgent {
     $this->browserVersion = $this->getBrowserVersion();
     $this->deviceType = $this->getDeviceType();
     $this->deviceBrand = $this->getDeviceBrand();
-    $this->referrer = $this->getReferrer();
-    $this->isReferred = isset($_SERVER['HTTP_REFERER']) ? TRUE : FALSE;
+    $this->referrer = (isset($userAgent) ? FALSE : $this->getReferrer());
+    $this->isReferred = (isset($userAgent) ? FALSE : (isset($_SERVER['HTTP_REFERER']) ? TRUE : FALSE));
   }
 
   /**
-  * Retrieve user ip address.
-  *
-  * @return string|null
-  */
+   * Retrieve user ip address.
+   *
+   * @return string|null
+   */
   private function getIP() {
     if(isset($_SERVER['HTTP_X_REAL_IP'])) {
       return $_SERVER['HTTP_X_REAL_IP'];
@@ -61,16 +61,16 @@ class UserAgent {
   }
 
   /**
-  * Parse os from userAgent
-  *
-  * @return string|null
-  */
+   * Parse os from user agent string
+   *
+   * @return string|null
+   */
   private function getOS() {
-    //List of all OS
+    // List of all OS
     $data = [
-      '/android/i' => 'Android',
+      '/Android/i' => 'Android',
       '/windows|win32/i' => 'Windows',
-      '/iphone|ipod|ipad|iOS/i' => 'iOS',
+      '/iPhone|iPod|iPad|iOS/i' => 'iOS',
       '/macintosh|mac os x|mac_powerpc/i' => 'Mac OS',
       '/blackberry|BB/i' => 'BlackBerry',
       '/webos/i' => 'Mobile',
@@ -78,7 +78,7 @@ class UserAgent {
       '/linux/i' => 'Linux',
       '/unix/i' => 'Unix',
     ];
-    //Match os from user agent
+    // Match os from user agent string
     foreach($data as $pattern => $os) {
       if(preg_match($pattern, $this->userAgent)) {
         return $os;
@@ -88,16 +88,19 @@ class UserAgent {
   }
 
   /**
-  * Parse os version from userAgent
-  *
-  * @return string
-  */
+   * Parse os version from userAgent
+   *
+   * @return string
+   */
   private function getOsVersion() {
     if($this->os == 'Android') {
-      //List of all android version
+      // List of all android version
       $data = [
+        '/android 12/i' => 'Android 12',
+        '/android 11/i' => 'Android 11',
         '/android 10/i' => '10 Q',
         '/android 9.0/i' => '9.0 Pie',
+        '/android 9/i' => '9 Pie',
         '/android 8.1.0/i' => '8.1.0 Oreo',
         '/android 8.1/i' => '8.1 Oreo',
         '/android 8.0/i' => '8.0 Oreo',
@@ -110,32 +113,94 @@ class UserAgent {
         '/android 5.1.1/i' => '5.1.1 Lollipop',
         '/android 5.1/i' => '5.1 Lollipop',
         '/android 5.0.2/i' => '5.0.2 Lollipop',
+        '/android 5.0.1/i' => '5.0.1 Lollipop',
         '/android 5.0/i' => '5.0 Lollipop',
         '/android 4.4.4/i' => '4.4.4 KitKat',
+        '/android 4.4.3/i' => '4.4.3 KitKat',
         '/android 4.4.2/i' => '4.4.2 KitKat',
+        '/android 4.4.1/i' => '4.4.1 KitKat',
         '/android 4.4/i' => '4.4 KitKat',
+        '/android 4.3.1/i' => '4.3.1 Jelly Bean',
         '/android 4.3/i' => '4.3 Jelly Bean',
         '/android 4.2.2/i' => '4.2.2 Jelly Bean',
+        '/android 4.2.1/i' => '4.2.1 Jelly Bean',
         '/android 4.2/i' => '4.2 Jelly Bean',
-        '/android 4.0.4/i' => '4.0.4 IceCream Sandwich',
-        '/android 4.0/i' => '4.0 IceCream Sandwich',
+        '/android 4.1.2/i' => '4.1.2 Jelly Bean',
+        '/android 4.1.1/i' => '4.1.1 Jelly Bean',
         '/android 4.1/i' => '4.1 Jelly Bean',
+        '/android 4.0.4/i' => '4.0.4 IceCream Sandwich',
+        '/android 4.0.3/i' => '4.0.3 IceCream Sandwich',
+        '/android 4.0.2/i' => '4.0.2 IceCream Sandwich',
+        '/android 4.0.1/i' => '4.0.1 IceCream Sandwich',
+        '/android 4.0/i' => '4.0 IceCream Sandwich',
+        '/android 3.2.6/i' => '3.2.6 Honeycomb',
+        '/android 3.2.5/i' => '3.2.5 Honeycomb',
+        '/android 3.2.4/i' => '3.2.4 Honeycomb',
+        '/android 3.2.3/i' => '3.2.3 Honeycomb',
+        '/android 3.2.2/i' => '3.2.2 Honeycomb',
+        '/android 3.2.1/i' => '3.2.1 Honeycomb',
+        '/android 3.2/i' => '3.2 Honeycomb',
+        '/android 3.1/i' => '3.1 Honeycomb',
+        '/android 3.0/i' => '3.0 Honeycomb',
         '/android 2.3.7/i' => '2.3.7 Gingerbread',
-        '/android 2.3.7/i' => '2.3.6 Gingerbread',
+        '/android 2.3.6/i' => '2.3.6 Gingerbread',
+        '/android 2.3.5/i' => '2.3.5 Gingerbread',
+        '/android 2.3.4/i' => '2.3.4 Gingerbread',
+        '/android 2.3.3/i' => '2.3.3 Gingerbread',
+        '/android 2.3.2/i' => '2.3.2 Gingerbread',
+        '/android 2.3.1/i' => '2.3.1 Gingerbread',
         '/android 2.3/i' => '2.3 Gingerbread',
         '/android 2.2.3/i' => '2.2.3 Froyo',
+        '/android 2.2.2/i' => '2.2.2 Froyo',
+        '/android 2.2.1/i' => '2.2.1 Froyo',
         '/android 2.2/i' => '2.2 Froyo',
+        '/android 2.1/i' => '2.1 Eclair',
+        '/android 2.0.1/i' => '2.0.1 Eclair',
+        '/android 2.0/i' => '2.0 Eclair',
+        '/android 1.6/i' => '1.6 Donut',
+        '/android 1.5/i' => '1.5 Cupcake',
+        '/android 1.1/i' => '1.1',
+        '/android 1.0/i' => '1.0',
       ];
-      //Match android version
+      // Match android version
       foreach($data as $pattern => $version) {
         if(preg_match($pattern, $this->userAgent)) {
           return $version;
         }
       }
     } else if($this->os == 'iOS') {
-      //List of all iOS version
+      // List of all iOS version
       $data = [
-        '/OS 13/i' => 'iOS 13',
+        '/OS 14_6/i' => 'iOS 14.6',
+        '/OS 14_5_1/i' => 'iOS 14.5.1',
+        '/OS 14_5/i' => 'iOS 14.5',
+        '/OS 14_4_2/i' => 'iOS 14.4.2',
+        '/OS 14_4_1/i' => 'iOS 14.4.1',
+        '/OS 14_4/i' => 'iOS 14.4',
+        '/OS 14_3/i' => 'iOS 14.3',
+        '/OS 14_2_1/i' => 'iOS 14.2.1',
+        '/OS 14_2/i' => 'iOS 14.2',
+        '/OS 14_1/i' => 'iOS 14.1',
+        '/OS 14_0_1/i' => 'iOS 14.0.1',
+        '/OS 14_0/i' => 'iOS 14.0',
+        '/OS 13_7/i' => 'iOS 13.7',
+        '/OS 13_6_1/i' => 'iOS 13.6.1',
+        '/OS 13_6/i' => 'iOS 13.6',
+        '/OS 13_5_1/i' => 'iOS 13.5.1',
+        '/OS 13_5/i' => 'iOS 13.5',
+        '/OS 13_4_1/i' => 'iOS 13.4.1',
+        '/OS 13_4/i' => 'iOS 13.4',
+        '/OS 13_3_1/i' => 'iOS 13.3.1',
+        '/OS 13_3/i' => 'iOS 13.3',
+        '/OS 13_2_3/i' => 'iOS 13.2.3',
+        '/OS 13_2_2/i' => 'iOS 13.2.2',
+        '/OS 13_2_1/i' => 'iOS 13.2.1',
+        '/OS 13_2/i' => 'iOS 13.2',
+        '/OS 13_1_3/i' => 'iOS 13.1.3',
+        '/OS 13_1_2/i' => 'iOS 13.1.2',
+        '/OS 13_1_1/i' => 'iOS 13.1.1',
+        '/OS 13_1/i' => 'iOS 13.1',
+        '/OS 13_0/i' => 'iOS 13.0',
         '/OS 12_0/i' => 'iOS 12',
         '/OS 12/i' => 'iOS 12',
         '/OS 11_0/i' => 'iOS 11',
@@ -204,14 +269,14 @@ class UserAgent {
         '/iPhone/i' => 'iOS',
         '/iPad/i' => 'iOS',
       ];
-      //Match iOS version
+      // Match iOS version
       foreach($data as $pattern => $version) {
         if(preg_match($pattern, $this->userAgent)) {
           return $version;
         }
       }
     } else if($this->os == 'Windows') {
-      //List of all windows version
+      // List of all windows version
       $data = [
         '/windows nt 10.0/i' => 'Windows 10',
         '/windows nt 6.2/i' => 'Windows 8',
@@ -226,14 +291,14 @@ class UserAgent {
         '/win95/i' => 'Windows 95',
         '/win16/i' => 'Windows 3.11',
       ];
-      //Match windows version
+      // Match windows version
       foreach($data as $pattern => $version) {
         if(preg_match($pattern, $this->userAgent)) {
           return $version;
         }
       }
     } else {
-      //Other os version
+      // Other os version
       $data = [
         '/macintosh|mac os x/i' => 'Mac OS X',
         '/mac_powerpc/i' => 'Mac OS 9',
@@ -251,12 +316,12 @@ class UserAgent {
   }
 
   /**
-  * Parse browser from userAgent
-  *
-  * @return string|null
-  */
+   * Parse browser from user agent string
+   *
+   * @return string|null
+   */
   private function getBrowser() {
-    //List of all browsers
+    // List of all browsers
     $data = [
       '/msie/i' => 'Internet Explorer',
       '/edge/i' => 'Edge',
@@ -274,7 +339,7 @@ class UserAgent {
       '/safari/i' => 'Safari',
       '/mobile/i' => 'Handheld Browser'
     ];
-    //Match browser
+    // Match browser
     foreach($data as $pattern => $browser) {
       if(preg_match($pattern, $this->userAgent)) {
         return $browser;
@@ -284,20 +349,20 @@ class UserAgent {
   }
 
   /**
-  * Parse browser version from userAgent
-  *
-  * @return string|null
-  */
+   * Parse browser version from user agent string
+   *
+   * @return string|null
+   */
   private function getBrowserVersion() {
     $browser = $this->browser;
     $known = array('version', $browser, 'other');
     $pattern = '#(?<browser>'. join('|', $known).')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
 
     if (!preg_match_all($pattern, $this->userAgent, $matches)) {
-      //We have no matching number just continue
+      // We have no matching number just continue
     }
 
-    //Get browser version from matched pattern
+    // Get browser version from matched pattern
     $i = count($matches['browser']);
     if($i != 1) {
       if(strripos($this->userAgent, 'version') < strripos($this->userAgent, $browser)) {
@@ -317,12 +382,12 @@ class UserAgent {
   }
 
   /**
-  * Parse device type from userAgent
-  *
-  * @return string|null
-  */
+   * Parse device type from user agent string
+   *
+   * @return string|null
+   */
   private function getDeviceType() {
-    //List of all device types
+    // List of all device types
     $data = [
       '/blackberry|BB/i' => 'Phone',
       '/android/i' => 'Phone',
@@ -335,7 +400,7 @@ class UserAgent {
       '/macintosh|mac os x|mac_powerpc/i' => 'Computer',
       '/linux|unix/i' => 'Computer',
     ];
-    //Match device type
+    // Match device type
     foreach($data as $pattern => $type) {
       if(preg_match($pattern, $this->userAgent)) {
         return $type;
@@ -345,15 +410,14 @@ class UserAgent {
   }
 
   /**
-  * Parse device brand from userAgent
-  *
-  * @return string|null
-  */
+   * Parse device brand from userAgent
+   *
+   * @return string|null
+   */
   private function getDeviceBrand() {
-    //List of all device brands
+    // List of all device brands
     $data = [
-      '/iPhone|iPad|iPod|Apple/i' => 'Apple',
-      '/macintosh|Apple/i' => 'Apple',
+      '/iPhone|iPad|iPod|macintosh|Apple/i' => 'Apple',
       '/SAMSUNG|SM-/i' => 'Samsung',
       '/Sony/i' => 'Sony',
       '/LG/i' => 'LG',
@@ -383,7 +447,7 @@ class UserAgent {
       '/Tecno/i' => 'Tecno',
       '/ZTE/i' => 'ZTE',
     ];
-    //Match device brand
+    // Match device brand
     foreach($data as $pattern => $brand) {
       if(preg_match($pattern, $this->userAgent)) {
         return $brand;
@@ -393,10 +457,10 @@ class UserAgent {
   }
 
   /**
-  * Checking user is referred or not
-  *
-  * @return string|null
-  */
+   * Checking user is referred or not
+   *
+   * @return string|null
+   */
   private function getReferrer() {
     return isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : NULL;
   }
